4cf6f45208fb567ba916919bc783f27630d8b3b9
politicalscience101 <politicalsciencecore101@gmail.com>
2024-09-15 12:57:21 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('btn').addEventListener('click', function() {
+  const heading = document.getElementById('heading');
+  heading.innerText = "Button Clicked!";
+  heading.style.color = 'blue';
+});
